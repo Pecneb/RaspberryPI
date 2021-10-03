244b433de8850b27b805a9b82ca09e4831a07b6d
Pecneb <57943524+Pecneb@users.noreply.github.com>
2021-10-03 13:17:37 +0200

    Delete web/raspberrypi directory

--- web/raspberrypi/piSec/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Notification(models.Model):
-    # notification date
-    ndate = models.DateTimeField('date time of notification')
-    # nid
-    nid = models.Index
