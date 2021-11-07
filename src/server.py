b7c87272bd5de540d009e69b5245972c99f3590a
Pecneb <ecneb2000@gmail.com>
2021-11-07 20:21:02 +0100

    Main module

---
+++ src/server.py
@@ -0,0 +1,32 @@
+from gpio_init import PIR, init, cleanup, LED
+import led_sensor as led
+import RPi.GPIO as gpio
+import alert
+import time
+
+gpio.setwarnings(False)
+
+def main():
+    # initialize the board
+    init()
+    
+    # begin the watch
+    try:
+        # start pir sensor, and look out for any motion in room
+        # when pir sends signal then start_alert is called
+        while True:
+            signal = gpio.wait_for_edge(PIR, gpio.RISING)
+            if signal == None:
+                led.on(LED)
+                time.sleep(1)
+                led.off(LED)
+                time.sleep(1)
+            else:
+                alert.start_alert()
+    # the program can be shut down with C^
+    except KeyboardInterrupt:
+        # reset pins
+        cleanup()
+
+if __name__ == "__main__":
+    main()
