e9444763c9b495060cc619dc015fbde3ecb2fdf9
tbalazs1213 <83310872+tbalazs1213@users.noreply.github.com>
2021-11-12 18:41:28 +0100

    Add files via upload

---
+++ src/alert.py
@@ -0,0 +1,16 @@
+from gpio_init import LED, BUZZER
+import led_sensor as led
+import buzzer_sensor as buzzer
+from auth import authenticate
+
+
+def start_alert():
+    '''
+    Starts alert, switches on led and buzzer.
+    '''
+    print("Motion detected!")
+    led.on(LED)
+    buzzer.on(BUZZER)
+    # event object should be created here
+    authenticate()
+    
