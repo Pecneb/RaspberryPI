da067a6b2b8cda5510d4c2562559a3adf58902ef
Pecneb <ecneb2000@gmail.com>
2022-02-15 23:22:10 +0100

    TODO: start.py with multiprocessing

---
+++ start.py
@@ -0,0 +1,8 @@
+from multiprocessing import Process
+
+"""
+    Start the system modules together.
+    To do this we will use multiprocessing.
+"""
+
+#TODO implement multiprocessing for the camera module, RPi.GPIO module and web module!
